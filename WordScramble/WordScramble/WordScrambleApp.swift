892f26d5a48913ec90f51c689fa93af1550465bf
Batuhan Yetgin <batuhan.yetgin@xinerji.com.tr>
2023-08-13 23:18:45 +0300

    WordScramble completed

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Batuhan Yetgin on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScramble/readme.md
@@ -0,0 +1,7 @@
+Topics
+- List
+- OnApper()
+- Bundle( load resourece from app bundle)
+- fatalError() 
+- Some string funcs ()
+- UITextChecker
